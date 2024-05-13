8c190492f646b79777316c0ef8e47abd05790a46
Nicolas <cotteznicolas@gmail.com>
2024-05-13 16:53:10 +0200

    v

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
     <li><a href="main.php">Accueil</a></li>
     <li><a href="ChercheTrajet.php">Chercher un trajet</a></li>
     <li><a href="PublierTrajet.php">Publier un trajet</a></li>
-    <li><a href="MesTrajets.php">Mes trajets</a></li>
+    <li><a href="MesTrajet.php">Mes trajets</a></li>
     <li><a href="MonProfile.php">Mon profil</a></li>
     <li><a href="DevenirConducteur.php">Devenir conducteur</a></li>
   </div>
