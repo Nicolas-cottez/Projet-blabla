7f3c34cf81d4019c4feff5852bffec58a99aa437
Nicolas <cotteznicolas@gmail.com>
2024-05-13 23:51:49 +0200

    ajout de login et sign in

---
+++ test.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<title>Webleb</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.9/css/unicons.css">
+	<script src="https://cdn.tailwindcss.com"></script>
+	<link rel="stylesheet" href="ex2.css">
+</head>
+
+<body>
+	<div class="section">
+		<div class="mx-auto">
+			<div class="full-height flex justify-center">
+				<div class="text-center self-center">
+					<div class="section pb-5 pt-5 pt-2 sm:pt-0 text-center">
+						<h6 class="mb-0 pb-3"><span>Log In</span><span>Sign Up</span></h6>
+						<input class="checkbox" type="checkbox" id="reg-log" name="reg-log" />
+						<label for="reg-log"></label>
+						<div class="card-3d-wrap mx-auto">
+							<div class="card-3d-wrapper">
+								<div class="card-front">
+									<div class="center-wrap">
+										<div class="section text-center">
+											<h4 class="mb-6 pb-4 font-medium text-3xl">Log In</h4>
+											<div class="form-group">
+												<input type="email" class="form-style" placeholder="Email">
+												<i class="input-icon uil uil-at"></i>
+											</div>
+											<div class="form-group mt-2">
+												<input type="password" class="form-style" placeholder="Password">
+												<i class="input-icon uil uil-lock-alt"></i>
+											</div>
+											<a href="#" class="btn mt-4">Login</a>
+											<p class="mb-0 mt-4 text-center"><a href="#" class="link">Forgot your password?</a></p>
+										</div>
+									</div>
+								</div>
+								<div class="card-back">
+									<div class="center-wrap">
+										<div class="section text-center">
+											<h4 class="mb-3 pb-3 font-medium text-3xl">Sign Up</h4>
+											<div class="form-group">
+												<input type="text" class="form-style" placeholder="Full Name">
+												<i class="input-icon uil uil-user"></i>
+											</div>
+											<div class="form-group mt-2">
+												<input type="tel" class="form-style" placeholder="Phone Number">
+												<i class="input-icon uil uil-phone"></i>
+											</div>
+											<div class="form-group mt-2">
+												<input type="email" class="form-style" placeholder="Email">
+												<i class="input-icon uil uil-at"></i>
+											</div>
+											<div class="form-group mt-2">
+												<input type="password" class="form-style" placeholder="Password">
+												<i class="input-icon uil uil-lock-alt"></i>
+											</div>
+											<div class="form-group mt-2">
+												<input type="file" class="form-style" placeholder="Upload Photo">
+												<i class="input-icon uil uil-upload"></i>
+											</div>
+											<a href="#" class="btn mt-4">Register</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
