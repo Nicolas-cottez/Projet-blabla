d86471d23186fc8a67818f8016239102dbdb8e18
marcoofrt <marco.frt99@gmail.com>
2024-05-15 13:15:41 +0200

    profil3

--- MonProfile.php
+++ MonProfile.php
@@ -1,21 +1,19 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script><!--inclu tailwind-->
-    <link rel="stylesheet" href="profil.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" /><!--inclu les fonts-->
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script> <!--inclu jquery-->
+    <link rel="stylesheet" href="MonProfile.css">
 
     <title>Mon profil</title>
 </head>
 <body>
 
-<h1>Marco Ferrante</h1>
+<h2>Marco Ferrante</h2>
 
+<div class = "UserPicture">
 <img src= "image/user.jpg" alt= "user">
+</div>
 
 
 </body>
