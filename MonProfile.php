c57a75dfdc2ab803e2336e9ac4eca33641ff2839
marcoofrt <marco.frt99@gmail.com>
2024-05-20 11:39:18 +0200

    mon profil : centré et mise en forme

--- MonProfile.php
+++ MonProfile.php
@@ -1,26 +1,24 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="MonProfile.css">
-
     <title>Mon profil</title>
 </head>
 <body>
-    <center>
-        <div class="box">
-            <img src="image/user.jpg" alt="user" class="UserPicture">
-            <input type="file" name="" id="file" accept="image/*">
-            <label for="file">EDIT PIC</label>
-            <input type="text" name="" placeholder="User Name">
-            <input type="email" name="" placeholder="Email ID">
-            <input type="text" name="" placeholder="PHONE NUMBER">
-            <input type="text" name="" placeholder="Date of Birth">
-            <input type="text" name="" placeholder="GENDER">
-            <button>CANCEL</button>
-            <button>DONE</button>
+    <div class="box">
+        <div class="UserPicture">
+            <img src="image/user.jpg" alt="user">
         </div>
-    </center>
+        <input type="file" name="" id="file" accept="image/*">
+        <label for="file">EDIT PIC</label>
+        <input type="text" name="" placeholder="User Name">
+        <input type="email" name="" placeholder="Email ID">
+        <input type="text" name="" placeholder="Phone Number">
+        <input type="text" name="" placeholder="Date of Birth">
+        <input type="text" name="" placeholder="Gender">
+        <button>CANCEL</button>
+        <button>DONE</button>
+    </div>
 </body>
 </html>
