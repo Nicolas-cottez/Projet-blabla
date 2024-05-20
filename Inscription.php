30c1e1cc4cfd202ed7d62c845aa41c118c19b637
marcoofrt <marco.frt99@gmail.com>
2024-05-20 23:31:17 +0200

    inscription php bdd

--- Inscription.php
+++ Inscription.php
@@ -5,16 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription - Blabla Omnes</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <script src="https://cdn.tailwindcss.com"></script><!--inclu tailwind-->
 </head>
 
-<body class="bg-gray-100">
-    <?php include 'Header.php'; ?>
-    <h1>Inscription</h1>
+<body>
 
-    
-    <?php include 'footer.php'; ?>
+<form method="POST" action="Inscription_2.php">
+    <label>Votre nom</label>
+    <input type="text" id="nom" name="nom" placeholder="Entrez votre nom..." required>
+    <br />
+    <label>Votre prénom</label>
+    <input type="text" id="Prenom" name="Prenom" placeholder="Entrez votre prénom..." required>
+    <br />
+    <label>Votre numéro de tél</label>
+    <input type="text" id="Num_Tel" name="Num_Tel" placeholder="Entrez votre numéro..." required>
+    <br />
+    <label>Votre email</label>
+    <input type="email" id="mail" name="mail" placeholder="Entrez votre email..." required>
+    <br />
+    <label>Votre mot de passe</label>
+    <input type="password" id="MDP" name="MDP" placeholder="Entrez votre mot de passe..." required>
+    <br />
+    <input type="submit" value="M'inscrire" name="ok">
+</form>
+
 </body>
 
 </html>
---
+++ Inscription_2.php
@@ -0,0 +1,43 @@
+<?php
+
+$source = "mysql:host=localhost;dbname=projet_blablacar";
+$login = "root";
+$mdp = "";
+
+try {
+    $db = new PDO($source, $login, $mdp);
+    echo "Vous êtes connecté !";
+} catch (PDOException $e) {
+    $error_message = $e->getMessage();
+    echo $error_message;
+    exit();
+}
+
+if (isset($_POST['ok'])) {
+    var_dump($_POST);
+    
+    // Récupération des données du formulaire
+    $nom = $_POST['nom'];
+    $Prenom = $_POST['Prenom'];
+    $Num_Tel = $_POST['Num_Tel'];
+    $mail = $_POST['mail'];
+    $MDP = $_POST['MDP'];
+
+    // Préparation de la requête SQL
+    $query = "INSERT INTO client (mail, MDP, nom, Num_Tel, Prenom) VALUES (:mail, :MDP, :nom, :Num_Tel, :Prenom)";
+    $stmt = $db->prepare($query);
+    
+    // Exécution de la requête avec les paramètres
+    $stmt->execute([
+        ':mail' => $mail,
+        ':MDP' => $MDP,
+        ':nom' => $nom,
+        ':Num_Tel' => $Num_Tel,
+        ':Prenom' => $Prenom
+    ]);
+
+    // Récupération et affichage des résultats
+    $reponse = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    var_dump($reponse);
+}
+?>
