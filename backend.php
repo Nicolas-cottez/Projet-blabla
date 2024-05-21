88a73c75c99a51795164656f4df446e9e051eafa
Nicolas <cotteznicolas@gmail.com>
2024-05-21 15:36:14 +0200

    changement

--- backend.php
+++ backend.php
@@ -5,7 +5,7 @@ $mdp = "";
 
 try {
     $db = new PDO($source, $login, $mdp);
-    echo "Vous êtes connecté !";
+    // echo "Vous êtes connecté !";
 } catch (PDOException $e) {
     $error_message = $e->getMessage();
     echo $error_message;
