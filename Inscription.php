65bd98c3892da1907be5f6410d3aa16706ac7825
Nicolas <cotteznicolas@gmail.com>
2024-05-21 17:58:51 +0200

    incription

--- Inscription.php
+++ Inscription.php
@@ -1,3 +1,42 @@
+<?php include 'backend.php'; ?>
+<?php
+if (isset($_POST['ok'])) {
+    var_dump($_POST);
+    // Récupération des données du formulaire
+    $nom = htmlspecialchars($_POST['nom']);
+    $Prenom = htmlspecialchars($_POST['Prenom']);
+    $Num_Tel = htmlspecialchars($_POST['Num_Tel']);
+    $mail = htmlspecialchars($_POST['mail']);
+    $MDP = sha1($_POST['MDP']);
+
+    $allowed_domains = ["omnesintervenant.com", "ece.fr", "edu.ece.fr"];
+    $email_domain = substr(strrchr($mail, "@"), 1);
+
+    if (!in_array($email_domain, $allowed_domains)) {
+        echo "L'adresse e-mail doit appartenir aux domaines suivants : omnesintervenant.com, ece.fr ou edu.ece.fr";
+        exit;
+    }
+
+    // Préparation de la requête SQL
+    $query = "INSERT INTO client (nom, Prenom, mail, MDP, Num_Tel) VALUES (:nom, :Prenom, :mail, :MDP, :Num_Tel)";
+    $stmt = $db->prepare($query);
+
+    // Exécution de la requête avec les paramètres
+    $stmt->execute([
+        ':nom' => $nom,
+        ':Prenom' => $Prenom,
+        ':mail' => $mail,
+        ':MDP' => $MDP,
+        ':Num_Tel' => $Num_Tel
+    ]);
+
+
+    // Récupération et affichage des résultats
+    $reponse = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    var_dump($reponse);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -9,7 +48,7 @@
 
 <body>
 
-<form method="POST" action="Inscription_2.php">
+<form method="POST" action="">
     <label>Votre nom</label>
     <input type="text" id="nom" name="nom" placeholder="Entrez votre nom..." required>
     <br />
