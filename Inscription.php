aca735e9726d6415ea984d56aa61dad92e72251a
Nicolas <cotteznicolas@gmail.com>
2024-05-21 19:56:29 +0200

    se conecter

--- Inscription.php
+++ Inscription.php
@@ -34,6 +34,9 @@ if (isset($_POST['ok'])) {
     // Récupération et affichage des résultats
     $reponse = $stmt->fetchAll(PDO::FETCH_ASSOC);
     var_dump($reponse);
+
+    header("location: SeConnecterTest.php");
+    exit();
 }
 ?>
 
@@ -48,31 +51,27 @@ if (isset($_POST['ok'])) {
 
 <body>
 
-<<<<<<< HEAD
-<form method="POST" action="">
-=======
-<form method="POST" action="Inscription_2.php" enctype="multipart/form-data">
->>>>>>> a6c4840121044abb3ab1cbefb2e7e2f5fa1f914c
-    <label>Votre nom</label>
-    <input type="text" id="nom" name="nom" placeholder="Entrez votre nom..." required>
-    <br />
-    <label>Votre prénom</label>
-    <input type="text" id="Prenom" name="Prenom" placeholder="Entrez votre prénom..." required>
-    <br />
-    <label>Votre photo</label>
-    <input type="file" id="Photo" name="Photo" required>
-    <br />
-    <label>Votre numéro de tél</label>
-    <input type="text" id="Num_Tel" name="Num_Tel" placeholder="Entrez votre numéro..." required>
-    <br />
-    <label>Votre email</label>
-    <input type="email" id="mail" name="mail" placeholder="Entrez votre email..." required>
-    <br />
-    <label>Votre mot de passe</label>
-    <input type="password" id="MDP" name="MDP" placeholder="Entrez votre mot de passe..." required>
-    <br />
-    <input type="submit" value="M'inscrire" name="ok">
-</form>
+    <form method="POST" action="">
+        <label>Votre nom</label>
+        <input type="text" id="nom" name="nom" placeholder="Entrez votre nom..." required>
+        <br />
+        <label>Votre prénom</label>
+        <input type="text" id="Prenom" name="Prenom" placeholder="Entrez votre prénom..." required>
+        <br />
+        <label>Votre photo</label>
+        <input type="file" id="Photo" name="Photo" required>
+        <br />
+        <label>Votre numéro de tél</label>
+        <input type="text" id="Num_Tel" name="Num_Tel" placeholder="Entrez votre numéro..." required>
+        <br />
+        <label>Votre email</label>
+        <input type="email" id="mail" name="mail" placeholder="Entrez votre email..." required>
+        <br />
+        <label>Votre mot de passe</label>
+        <input type="password" id="MDP" name="MDP" placeholder="Entrez votre mot de passe..." required>
+        <br />
+        <input type="submit" value="M'inscrire" name="ok">
+    </form>
 
 </body>
 
