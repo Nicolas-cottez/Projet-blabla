ee29383dc2cc2764de0e6bf92d798659f4328df3
Mattymacouille <matteo.devaux01@gmail.com>
2024-05-22 09:59:50 +0200

    liste déroulante

--- ChercheTrajet.php
+++ ChercheTrajet.php
@@ -15,7 +15,7 @@
         <form id="searchForm" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <div class="mb-4">
                 <label for="departure" class="block text-gray-700 text-sm font-bold mb-2">Départ:</label>
-                <select name="departure" id="departure" placeholder="Ville d'arrivée" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                <select name="departure" id="departure" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
                     <?php
                     $reponse = $db->query('SELECT adresse FROM campus');
                     while ($donnees = $reponse->fetch()) {
@@ -25,7 +25,7 @@
                         </option>
                     <?php
                     }
-                    $reponse->closeCursor();    
+                    $reponse->closeCursor(); // Fermer la requête une fois terminée
                     ?>
                 </select>
             </div>
@@ -50,4 +50,3 @@
 
     <script src="search.js"></script>
    
->
