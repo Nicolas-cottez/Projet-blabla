5680bc7935cbf11649d4776e3304d9534f003950
Alexis-guerin <alexis.guerin@edu.ece.fr>
2024-05-24 19:05:52 +0200

    Ajout de fichiers pour la gestion des trajets

---
+++ annuler_trajet.php
@@ -0,0 +1,26 @@
+<?php
+include 'backend.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = $_POST['ID_trajet'] ?? null;
+
+    // Vérifiez que l'ID est valide
+    if (!empty($id) && is_numeric($id)) {
+        try {
+            $requete = $db->prepare("DELETE FROM trajet WHERE ID_trajet = :id");
+            $requete->bindParam(':id', $id, PDO::PARAM_INT);
+            $requete->execute();
+
+            // Redirige vers la page des trajets après la suppression
+            header("Location: mestrajets.php");
+            exit();
+        } catch (PDOException $e) {
+            echo "Erreur : " . $e->getMessage();
+        }
+    } else {
+        echo "ID invalide.";
+    }
+} else {
+    echo "Méthode de requête non autorisée.";
+}
+?>
